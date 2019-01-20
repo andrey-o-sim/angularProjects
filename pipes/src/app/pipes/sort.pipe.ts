7db03f46ac6ba98bcfc42135c45c1b1f228446d8
DESKTOP-ULQ3D3M\Andre <andriisymonenko@gmail.com>
2019-01-20 18:01:58 +0200

    Pipe assigment

---
+++ pipes/src/app/pipes/sort.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "sort"
+})
+export class SortPipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    if (value.length === 0) {
+      return value;
+    }
+    return value.sort((a, b) => {
+      var nameA = a.name.toUpperCase();
+      var nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+}
