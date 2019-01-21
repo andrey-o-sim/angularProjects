b024647d73b2d70007c6f644c8c6cc5aced8c3cd
DESKTOP-ULQ3D3M\Andre <andriisymonenko@gmail.com>
2019-01-21 21:01:21 +0200

    Http Main project

--- main-project/src/app/header/header.component.ts
+++ main-project/src/app/header/header.component.ts
@@ -1,7 +1,24 @@
 import { Component } from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
+import { Subscription } from "rxjs";
+import { Response } from "@angular/http";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html"
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private subscription: Subscription;
+
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.subscription = this.dataStorageService
+      .storeRecipes()
+      .subscribe((response: Response) => console.log(response));
+  }
+
+  onFetchData() {
+    this.dataStorageService.getRecipese();
+  }
+}
